d7d6d0787e752bd161009179276ab3e27d6321dd
Ayanfe <oluwafemi.wafi@gmail.com>
2021-11-27 10:26:56 +0000

    Authentication Endpoint

--- users-service/src/helpers/passwordCompareSync.ts
+++ users-service/src/helpers/passwordCompareSync.ts
@@ -1,1 +1,7 @@
-// import bcrypt from 
+import bcrypt from "bcryptjs";
+
+const passwordCompareSync = (passwordToTest: string, passwordHash: string) =>
+   bcrypt.compareSync(passwordToTest, passwordHash);
+
+   export default passwordCompareSync;
+   
--- users-service/src/server/routes.ts
+++ users-service/src/server/routes.ts
@@ -1,10 +1,17 @@
+import config from "config";
+import dayjs from "dayjs";
 import { Express } from "express";
-import { getRepository } from "typeorm";
+import { getConnection, getRepository } from "typeorm";
 
 import User from "#root/db/entities/User";
-import { nextTick } from "process";
+import UserSession from "#root/db/entities/UserSession";
+import generateUUID from "#root/helpers/generateUUID";
+import passwordCompareSync from "#root/helpers/passwordCompareSync";
+
+const USER_SESSION_EXPIRY_HOURS = <number>config.get("USER_SESSION_EXPIRY_HOURS");
 
 const setupRoutes = (app: Express) => {
+   const connection = getConnection();
    const userRepository = getRepository(User);
 
    app.post("/sessions", async (req, res, next) => {
@@ -23,6 +30,24 @@ const setupRoutes = (app: Express) => {
          );
 
          if (!user) return next(new Error("Invalid username!"));
+
+         if (!passwordCompareSync(req.body.password, user.passwordHash)) {
+            return next(new Error("Inavalid Passwrrd!"));
+         }
+
+         const expiresAt = dayjs().add(USER_SESSION_EXPIRY_HOURS, "hour").toISOString();
+
+         const sessionToken = generateUUID();
+         
+         const userSession = {
+            expiresAt,
+            id: sessionToken,
+            userId: user.id
+         };
+
+         await connection.createQueryBuilder().insert().into(UserSession).values([userSession]);
+
+         return res.json(userSession);
       }  catch (err) {
          return next(err);
       }
